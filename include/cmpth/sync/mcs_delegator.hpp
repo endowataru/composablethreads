3a24b027440e9965e26e438c400c1ea108b74ba4
Wataru ENDO <wendo@eidos.ic.i.u-tokyo.ac.jp>
2019-08-22 14:27:55 +0900

    Unify implementation of delegators (mcs & ring_buf) in cmpth

--- include/cmpth/sync/mcs_delegator.hpp
+++ include/cmpth/sync/mcs_delegator.hpp
@@ -1,24 +1,25 @@
 
 #pragma once
 
-#include <cmpth/sync/basic_mcs_delegator.hpp>
+#include <cmpth/sync/basic_sync_delegator.hpp>
+#include <cmpth/sync/basic_mcs_queue.hpp>
 #include <cmpth/sync/basic_mcs_core.hpp>
-#include <cmpth/pool/basic_return_pool.hpp>
 #include <cmpth/sync/basic_uncond_thread.hpp>
 
 namespace cmpth {
 
 template <typename P>
-class basic_pool_mcs_delegator
-    : public basic_mcs_delegator<P>
+class basic_mcs_delegator
+    : public basic_sync_delegator<P>
 {
-public:
     using mcs_node_pool_type = typename P::mcs_node_pool_type;
+    
+public:
     // for compatibility
     using qdlock_pool_type = mcs_node_pool_type;
-    using qdlock_node_type = typename P::mcs_node_type;
+    using qdlock_node_type = typename P::sync_node_type;
     
-    explicit basic_pool_mcs_delegator(mcs_node_pool_type& pool)
+    explicit basic_mcs_delegator(mcs_node_pool_type& pool)
         : pool_(pool)
         // Note: GCC 4.8 cannot use {} for initializing a reference
     { }
@@ -31,13 +32,12 @@ private:
     mcs_node_pool_type& pool_;
 };
 
-
 template <typename UltItf, typename P2>
-struct pool_mcs_delegator_node
+struct mcs_delegator_node
 {
     using uncond_var_type = typename UltItf::uncond_variable;
     using atomic_node_ptr_type =
-        typename UltItf::template atomic<pool_mcs_delegator_node*>;
+        typename UltItf::template atomic<mcs_delegator_node*>;
     using delegated_func_type = typename P2::delegated_func_type;
     
     atomic_node_ptr_type    next;
@@ -45,13 +45,9 @@ struct pool_mcs_delegator_node
     delegated_func_type     func;
 };
 
-
 template <typename UltItf, typename P2>
-class pool_mcs_delegator_pool;
-
-template <typename UltItf, typename P2>
-struct pool_mcs_delegator_pool_policy {
-    using element_type = pool_mcs_delegator_node<UltItf, P2>;
+struct mcs_delegator_pool_policy {
+    using element_type = mcs_delegator_node<UltItf, P2>;
     
     template <typename Pool>
     static fdn::size_t get_pool_threshold(Pool& pool) {
@@ -68,27 +64,28 @@ struct pool_mcs_delegator_pool_policy {
 };
 
 template <typename UltItf, typename P2>
-struct pool_mcs_delegator_policy
+struct mcs_delegator_policy
 {
-    using derived_type = basic_pool_mcs_delegator<pool_mcs_delegator_policy>;
-    using mcs_core_type = basic_mcs_core<pool_mcs_delegator_policy>;
-    using mcs_node_type = pool_mcs_delegator_node<UltItf, P2>;
+    using derived_type = basic_mcs_delegator<mcs_delegator_policy>;
+    using mcs_core_type = basic_mcs_core<mcs_delegator_policy>;
+    using sync_queue_type = basic_mcs_queue<mcs_delegator_policy>;
+    
+    using sync_node_type = mcs_delegator_node<UltItf, P2>;
+    using mcs_node_type = sync_node_type; // TODO: for compatibility
     using atomic_node_ptr_type = typename mcs_node_type::atomic_node_ptr_type;
     using ult_itf_type = UltItf;
     using mcs_node_pool_type =
-        basic_ext_return_pool<UltItf, pool_mcs_delegator_pool_policy<UltItf, P2>>;
+        basic_ext_return_pool<UltItf, mcs_delegator_pool_policy<UltItf, P2>>;
     
-    using qdlock_thread_type = uncond_qdlock_thread<pool_mcs_delegator_policy>;
+    using qdlock_thread_type = uncond_qdlock_thread<mcs_delegator_policy>;
     
     using assert_policy_type = typename UltItf::assert_policy;
     using log_policy_type = typename UltItf::log_policy;
 };
 
 template <typename UltItf, typename P2>
-using pool_mcs_delegator =
-    basic_pool_mcs_delegator<
-        pool_mcs_delegator_policy<UltItf, P2>
-    >;
+using mcs_delegator =
+    basic_mcs_delegator<mcs_delegator_policy<UltItf, P2>>;
 
 } // namespace cmpth
 
